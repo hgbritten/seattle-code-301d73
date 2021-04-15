8efd58d1c4d394040f5f4660a783c4257a71871c
hunter britten <huntergbritten@gmail.com>
2021-04-15 13:27:39 -0700

    demo4

--- class-04/demo/form-demo/in-form-ation/src/App.js
+++ class-04/demo/form-demo/in-form-ation/src/App.js
@@ -1,59 +1,130 @@
-import React from 'react';
-import Header from './Header.js';
+// import React from 'react';
+// import Header from './Header.js';
 
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import './App.css';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import './App.css';
 
-class App extends React.Component {
+// class App extends React.Component {
 
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       username: 'user'
+//     };
+//   }
+//   handleFormSubmitted = e => {
+//     e.preventDefault();
+//     console.log('submitted');
+//   }
+//   handleUsernameTyped = e => {
+//     // e.preventDefault();
+//     if(e.target.value.length > 0) {
+//       this.setState({
+//         username: e.target.value
+//       });
+//     } else {
+//       this.setState({
+//         username: 'user'
+//       });
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Header potato={this.state.username}/>
+//         <form onSubmit={this.handleFormSubmitted}>
+//           <label htmlFor="name">Name</label>
+
+//           <input id="name" name="name" type="text" onInput={this.handleUsernameTyped} />
+//           <br />
+//           <label htmlFor="dob">Date of Birth</label>
+//           <input type="date" name="dob" id="dob" />
+//           <br />
+//           <input type="submit" />
+//         </form>
+//         <Container>
+//           <Form>
+//             <Form.Group controlId="bootstrapName">
+//               <Form.Label>Name</Form.Label>
+//               <Form.Control type="text" onInput={this.handleUsernameTyped} />
+//             </Form.Group>
+//           </Form>
+//         </Container>
+//       </div>
+//     )
+//   }
+// }
+// export default App;
+
+import { Component } from 'react';
+
+export default class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      username: 'user'
-    };
-  }
-  handleFormSubmitted = e => {
-    e.preventDefault();
-    console.log('submitted');
-  }
-  handleUsernameTyped = e => {
-    // e.preventDefault();
-    if(e.target.value.length > 0) {
-      this.setState({
-        username: e.target.value
-      });
-    } else {
-      this.setState({
-        username: 'user'
-      });
+      user: '',
     }
   }
+
+  changeHandler = event => {
+    const newNameText = event.target.value;
+    this.setState({
+      user: newNameText
+
+    })
+  }
+
   render() {
     return (
-      <div>
-        <Header potato={this.state.username}/>
-        <form onSubmit={this.handleFormSubmitted}>
-          <label htmlFor="name">Name</label>
+      <>
+        <Header user={this.state.user} />
+        <Form user={this.state.user} changeHandler={this.changeHandler} />
+        <Slim user={this.state.user} changeHandler={this.changeHandler} />
+      </>
+    );
+  }
+}
 
-          <input id="name" name="name" type="text" onInput={this.handleUsernameTyped} />
-          <br />
-          <label htmlFor="dob">Date of Birth</label>
-          <input type="date" name="dob" id="dob" />
-          <br />
-          <input type="submit" />
-        </form>
-        <Container>
-          <Form>
-            <Form.Group controlId="bootstrapName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" onInput={this.handleUsernameTyped} />
-            </Form.Group>
-          </Form>
-        </Container>
-      </div>
+class Header extends Component {
+
+  render() {
+    return (
+      <h2> Hello, {this.props.user || 'user'}! </h2>
     )
   }
 }
-export default App;
+
+class Form extends Component {
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.props.changeHandler} value={this.props.user} type='text' />
+      </form>
+    )
+  }
+}
+
+class Slim extends Component {
+
+  // this.changeHandler = this.changeHandler.bind(this);
+
+
+  // changeHandler(event) {
+  //   const newNameText = event.target.value;
+  //   this.setState({
+  //     name: newNameText
+
+  //   })
+  // }
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.props.changeHandler} value={this.props.user} type='text' />
+      </form>
+    )
+  }
+}
