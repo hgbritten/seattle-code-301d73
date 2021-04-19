f7999eefe3b6ede0c3a394aea759a458b1d222d1
hunter britten <huntergbritten@gmail.com>
2021-04-19 13:49:47 -0700

    day6

--- class-06/demo/location/src/app.js
+++ class-06/demo/location/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 class App extends React.Component {
   constructor(props){
@@ -10,12 +10,36 @@ class App extends React.Component {
     }
   }
 
-  getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-    const res = await axios.get(API);
-    console.log(res.data[0])
-    this.setState({ location:res.data[0] });
-  }
+getLocation = async () => {
+  // return alert(this.state.searchQuery);
+
+  // Danger: only for a moment will we put key directly in code
+  const apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+
+  console.log(apiUrl);
+/*
+  const response = await axios.get(apiUrl); 
+
+  const location = response.data[0];
+
+  this.setState({
+    location,
+    // or location: location,
+  });
+
+  const img_url=`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
+
+  console.log(img_url);
+*/
+}
+
+
+  // getLocation = async () => {
+  //   const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+  //   const res = await axios.get(API);
+  //   console.log(res.data[0])
+  //   this.setState({ location:res.data[0] });
+  // }
 
   render() {
     return(
